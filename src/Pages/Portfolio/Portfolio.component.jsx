dd0128b6cac8d3789d621a507b0144143850ad76
dorjepadma <dorjepadma@gmail.com>
2022-10-13 16:20:42 -0700

    updated landing page

--- src/Pages/Portfolio/Portfolio.component.jsx
+++ src/Pages/Portfolio/Portfolio.component.jsx
@@ -6,45 +6,51 @@ import {
   MDBContainer,
 } from 'mdb-react-ui-kit';
 
-import Nestz from '../../assets/images/Nestz.png';
+import Nestz from '../../assets/images/Nestz.jpg';
 import Galaxy from '../../assets/images/beautifulGalaxy.jpeg';
+import Hung from '../../assets/images/hung-1.jpg';
+
+import './Portfolio.styles.scss'
 
 const Portfolio = () => {
   return (
     <MDBContainer className='portfolioContainer'>
-    <MDBCarousel showControls showIndicators >
+    <MDBCarousel showControls showIndicators className='portfolioCarousel'>
       <MDBCarouselItem
-        className='w-100 d-block'
+        className='w-100  d-block'
         itemId={1}
         src={Nestz}
         alt='a tiny home'
       >
         <h5>NESTZ</h5>
         <p>An e-commerce website for tiny homes.</p>
-        <MDBBtn>
+        <MDBBtn color='white'>
           <a href='https://nestz.life/'>Visit Site</a>
         </MDBBtn>
       </MDBCarouselItem>
       <MDBCarouselItem
-        className='w-100 d-block'
+        className='w-100  d-block'
         itemId={2}
         src={Galaxy}
         alt='a beautiful galaxy'
       >
         <h5>Astral Tunes</h5>
         <p>A website that creates music from the position of the planets</p>
-        <MDBBtn>
+        <MDBBtn color='white'>
           <a href='https://astraltunes.com/'>Visit Site</a>
         </MDBBtn>
       </MDBCarouselItem>
       <MDBCarouselItem
-        className='w-100 d-block'
+        className='w-100  d-block'
         itemId={3}
-        src='https://mdbootstrap.com/img/new/slides/043.jpg'
-        alt='...'
+        src={Hung}
+        alt='a colorful painting'
       >
         <h5>Andrea Traber</h5>
         <p>An artist portfolio website.</p>
+        <MDBBtn color='white'>
+          <a href='https://andreatraber.com/'>Visit Site</a>
+        </MDBBtn>
       </MDBCarouselItem>
     </MDBCarousel>
     </MDBContainer>
